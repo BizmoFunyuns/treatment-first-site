283a2a51e899fcf46ed59ab6204b9c478c6df6d6
BizmoFunyuns <gmarkjunk@yahoo.com>
2015-09-21 18:58:11 -0500

    script cleanup, refactor resizeMenu; change css

--- js/site.js
+++ js/site.js
@@ -1,14 +1,18 @@
 "use strict";
+var resizeMenu = function () {
+        $('#float-nav-bar').addClass('fixed').css("width", $("#container").css("width"));
+    };
+
 $(document).ready(function () {
     $('#main').load('home.html');
 
     var topOfNav = $('#float-nav-bar').offset().top;
 
     $(window).scroll(function () {
-
         if ($(this).scrollTop() > topOfNav) {
             $('#nav-bar-placeholder').show();
-            $('#float-nav-bar').addClass('fixed').css("width", $("#container").css("width"));
+            
+            resizeMenu();
         }
         else {
             $('#nav-bar-placeholder').hide();
@@ -17,36 +21,17 @@ $(document).ready(function () {
     });
     
     $(window).resize( function() {
-        console.log("resizing");
+        resizeMenu();
     });
 
     $('.dropdown').hover(
         function () {
-            var parentLiWidth           = $(this).outerWidth(true);
-            var detoxServLeft           = $('#detox-services').position().left;
-            var parentLiDropdownLeft    = $(this).position().left;
-            var parentLiDropdownWidth   = $(this).width();
-            //var offset = (parentLiDropdownWidth - $('#detox-services').width()) / 2 - parseFloat($('#detox-services').css('margin-left')
-            //                             .replace("px", ""));
+            var parentLiDropdownWidth = $(this).width();
             
             var offset = (parentLiDropdownWidth - $(this).children('a').width()) / 2 - parseFloat($(this).children('a').css('margin-left')
                                          .replace("px", ""));
-            
-            $(this).children('a').width()
-            
-            //console.log("foo: " + foo);
-            
-            //console.log("detoxServicesLeft: " + detoxServLeft);
-            
-            //console.log("detox-services width: " + parseInt($('#detox-services').width()));
-            
-            //var offset = $('#detox-services').outerWidth(true) - parseInt($('#detox-services').width());
-            
-            //var subMenuLeft = parseFloat($('#detox-services').css('margin-left').replace("px", "")) + parseFloat($('#detox-services').css('padding-left').replace("px", ""));
-            
+
             $(this).children('.sub-menu').slideDown(200).css('left', offset);
-            
-            //$("#mydiv").css({top: 200, left: 200, position:'absolute'});
             
         },
         function () {
@@ -59,5 +44,5 @@ $(document).ready(function () {
         var page = $(this).attr('href');
         $('#main').load(page);
     });
-
 });
+
