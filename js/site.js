329ae8cbb268500ada99073e51309eb7906ece54
BizmoFunyuns <gmarkjunk@yahoo.com>
2015-10-24 21:56:36 -0500

    JavaScript cleanup

--- js/site.js
+++ js/site.js
@@ -1,56 +1,52 @@
-"use strict";
-var resizeMenu = function () {
-        $('#float-nav-bar').css("width", $("#container").css("width"));
-    };
+/*global $, jQuery, alert*/
+(function () {
+    "use strict";
+    var resizeMenu = function () {
+            $('#float-nav-bar').css("width", $("#container").css("width"));
+        };
 
-$(document).ready(function () {
-    $('#main').load('home.html');
+    $(document).ready(function () {
+        $('#main').load('home.html');
 
-    var topOfNav = $('#float-nav-bar').offset().top;
+        var topOfFloatNavBar = $('#float-nav-bar').offset().top;
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > topOfNav) {
-            $('#nav-bar-placeholder').show();
-            $('#float-nav-bar').addClass('fixed');
-            
-            resizeMenu();
-        }
-        else {
-            $('#nav-bar-placeholder').hide();
-            $('#float-nav-bar').removeClass('fixed');
-        }
-    });
-    
-    $(window).resize( function() {
-        resizeMenu();
-    });
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > topOfFloatNavBar) {
+                $('#nav-bar-placeholder').show();
+                $('#float-nav-bar').addClass('fixed');
 
-    $('.dropdown').hover( function () {
-            var parentLiDropdownWidth   = $(this).width();
-            var menuLinkWidth           = $(this).children('a').width();
-            var menuLinkProps = {
-                width:      $(this).children('a').width(),
-                marginLeft: parseFloat($(this).children('a').css('margin-left').replace("px", ""))
+                resizeMenu();
+            } else {
+                $('#nav-bar-placeholder').hide();
+                $('#float-nav-bar').removeClass('fixed');
             }
-                         
-            
-            //var offset = (parentLiDropdownWidth - menuLinkWidth) / 2 - parseFloat($(this).children('a').css('margin-left')
-            //                             .replace("px", ""));
-                         
-            var offset = (parentLiDropdownWidth - menuLinkProps.width) / 2 - menuLinkProps.marginLeft;
+        });
+
+        $(window).resize(function () {
+            resizeMenu();
+        });
+
+        $('.dropdown').hover(function () {
+            var parentLiDropdownWidth   = $(this).width(),
+                menuLinkWidth           = $(this).children('a').width(),
+                menuLinkProps = {
+                    width:      $(this).children('a').width(),
+                    marginLeft: parseFloat($(this).children('a').css('margin-left').replace("px", ""))
+                },
+                offset = (parentLiDropdownWidth - menuLinkProps.width) / 2 - menuLinkProps.marginLeft;
 
             $(this).children('.sub-menu').slideDown(200).css('left', offset);
-            
-        },
-        function () {
-            $(this).children('.sub-menu').slideUp(200);
-        }
-    );
 
-    $('li a, h1 a').on('click', function (e) {
-        e.preventDefault();
-        var page = $(this).attr('href');
-        $('#main').load(page);
+        },
+            function () {
+                $(this).children('.sub-menu').slideUp(200);
+            });
+
+        $('li a, h1 a').on('click', function (e) {
+            e.preventDefault();
+            var page = $(this).attr('href');
+            $('#main').load(page);
+        });
     });
-});
+}());
 
