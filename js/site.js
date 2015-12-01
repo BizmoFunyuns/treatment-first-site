7d0c2086c2c654e60492585d15e77207256e68f9
BizmoFunyuns <gmarkjunk@yahoo.com>
2015-11-30 21:53:18 -0600

    Add blog page, doctor pic; general changes

--- js/site.js
+++ js/site.js
@@ -3,7 +3,7 @@
     "use strict";
     
     var resizeMenu = function () {
-            $('#float-nav-bar').css("width", $("#container").css("width"));
+            $('#float-nav-bar').css('width', $('.container').css('width'));
         };
 
     $(document).ready(function () {
@@ -43,7 +43,7 @@
                 $(this).children('.sub-menu').slideUp(200);
             });
 
-        $('li a, h1 a').on('click', function (e) {
+        $('li a, h1 a').not('.own-page').on('click', function (e) {
             e.preventDefault();
             var page = $(this).attr('href');
             $('#main').load(page);
