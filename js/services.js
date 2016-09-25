e3586295c584109f684e7221f9e310610070088a
BizmoFunyuns <gmarkjunk@yahoo.com>
2016-09-25 16:29:30 -0500

    Add Services structured data

---
+++ js/services.js
@@ -0,0 +1,11 @@
+/*global $, jQuery, alert*/
+(function () {
+    "use strict";
+
+     "@context": "http://schema.org",
+     "@type": "WebPage",
+     "title": "Services",
+     "sub-title": "Your Journey to Sobriety Starts Here",
+     "mainContent": "Whether you are the one seeking help, or a friend or family member seeking help for a loved one, we’re here to assist you. Our goal is to make your rehabilitation planning process as stress free as possible. Relax, let us take care of the details."
+}());
+
